12f5c1a755425cd8ce2cb9c213a6f4cdac688f28
Jose <josmamogut@gmail.com>
2026-04-09 18:50:28 +0200

    modified:   analyze_pcap.py 	new file:   bbr.csv 	new file:   bbr.pcap 	new file:   bbr_graph.png 	new file:   cubic.csv 	new file:   cubic.pcap 	new file:   cubic_graph.png 	modified:   reno_graph.png 	modified:   steps.txt

--- analyze_pcap.py
+++ analyze_pcap.py
@@ -20,31 +20,33 @@ def extract_cwnd(pcap_file, csv_file):
     
     print("Data extracted in a csv")
     
-def plot_cwnd(csv_file, output_image):
-    print("Generating graph of congestion")
+def plot_single_cwnd(csv_file, algo_name, color, output_image):
+    print(f"Generating graph for {algo_name}")
     
-    df = pd.read_csv(csv_file, names=["Time", "Bytes_in_Flight"], on_bad_lines='skip')
-    df = df.dropna()
+    if not os.path.exists(csv_file):
+        print(f"File {csv_file} does not exist. Skipping.")
+        return
+        
+    plt.figure(figsize=(12, 6)) 
     
-    plt.figure(figsize=(12,6))
-    plt.plot(df["Time"], df["Bytes_in_Flight"], label="TCP Reno", color='blue', linewidth=1)
-    
-    plt.title("CWND - Reno")
-    plt.xlabel("Time (Seconds)")
+    df = pd.read_csv(csv_file, names=["Time", "Bytes_in_Flight"], on_bad_lines='skip').dropna()
+    plt.plot(df["Time"], df["Bytes_in_Flight"], label=f"TCP {algo_name}", color=color, linewidth=1.5)
+        
+    plt.title(f"CWND: {algo_name} - Coffee Shop Wi-Fi (4% Loss)")
+    plt.xlabel("Time (seconds)")
     plt.ylabel("Bytes in Flight")
     plt.grid(True, linestyle='--', alpha=0.7)
     plt.legend()
     
     plt.savefig(output_image, dpi=300)
+    plt.close()
     print(f"Graph saved as: {output_image}")
     
 if __name__ == "__main__":
-    pcap_name = "reno.pcap"
-    csv_name = "reno.csv"
-    img_name = "reno_graph.png"
+    extract_cwnd("reno.pcap", "reno.csv")
+    extract_cwnd("bbr.pcap", "bbr.csv")
+    extract_cwnd("cubic.pcap", "cubic.csv")
     
-    if os.path.exists(pcap_name):
-        extract_cwnd(pcap_name, csv_name)
-        plot_cwnd(csv_name, img_name)
-    else:
-        print(f"Error: File not found")
+    plot_single_cwnd("reno.csv", "Reno", "blue", "reno_graph.png")
+    plot_single_cwnd("bbr.csv", "BBR", "red", "bbr_graph.png")
+    plot_single_cwnd("cubic.csv", "CUBIC", "green", "cubic_graph.png")
